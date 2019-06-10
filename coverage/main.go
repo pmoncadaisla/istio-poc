28537f6f697d7451d8436b08ff7d84c8a72c30c2
Pablo Moncada <pablo.moncada@masmovil.com>
2019-06-10 10:46:44 +0200

    Added coverage

---
+++ coverage/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/check", func(c *gin.Context) {
+		_, err := http.Get("http://iam/check")
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err,
+			})
+		}
+		c.JSON(200, gin.H{
+			"message": "Coverage found for address",
+		})
+	})
+	r.Run(":80") // listen and serve on 0.0.0.0:8080
+}
